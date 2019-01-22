edd23f131618f7cdc1c6ad2751f1f359de1f5bb5
Felipe Cabreira <felipe.cabreira@acad.pucrs.br>
2019-01-21 23:53:04 -0200

    Ternarios

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+
+// console.log('Hello World');
+
+
+// Ternario : 3 partes - Condição ? (if) : (else)
+var firstName = 'John';
+var age = 16;
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+if (age >= 18) {
+    var drink = 'beer';
+} else {
+    var drink = 'juice';
+}   
+
